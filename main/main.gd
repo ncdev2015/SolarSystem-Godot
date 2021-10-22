cf60c3727cfcb891a683c52159a797c402f69b3a
ncdev2015 <ncdev2015@gmail.com>
2021-10-22 15:10:28 -0300

    Updated: sun size and rotation

--- main/main.gd
+++ main/main.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var planets: Array
 var positions: Array
+var planets_to_rotate: Array
 
 var mercury = preload("res://planets/mercury.tscn")
 var venus = preload("res://planets/venus.tscn")
@@ -11,6 +12,8 @@ var jupiter = preload("res://planets/jupiter.tscn")
 var saturn = preload("res://planets/saturn.tscn")
 var neptune = preload("res://planets/neptune.tscn")
 var uranus = preload("res://planets/uranus.tscn")
+
+var sun_rotation_velocity = 0.25
 
 func _ready():
 	planets = [
@@ -25,23 +28,26 @@ func _ready():
 	]
 	
 	positions = [
-		$Positions/MercuryPosition,
-		$Positions/VenusPosition,
-		$Positions/EarthPosition,
-		$Positions/MarsPosition,
-		$Positions/JupiterPosition,
-		$Positions/SaturnPosition,
-		$Positions/NeptunePosition,
-		$Positions/UranusPosition
+		$Planets/MercuryPosition,
+		$Planets/VenusPosition,
+		$Planets/EarthPosition,
+		$Planets/MarsPosition,
+		$Planets/JupiterPosition,
+		$Planets/SaturnPosition,
+		$Planets/NeptunePosition,
+		$Planets/UranusPosition
 	]
 
 func _process(delta):
-	pass
+	$Sun.rotate(delta * sun_rotation_velocity)
+	for planet in planets_to_rotate:
+		# Add rotation
+		pass
 
 func _unhandled_input(event):
 	if event is InputEventKey:
 		if event.pressed and event.scancode == KEY_SPACE and not event.echo:
-			var next_planet = get_next_planet()			
+			var next_planet = get_next_planet()
 			if next_planet["planet"]:
 				add_planet_to_scene(next_planet)
 			
@@ -56,3 +62,5 @@ func add_planet_to_scene(planet):
 	instance.position = planet["position"].position
 	instance.scale = Vector2(.1,.1)
 	add_child(instance)
+	
+	planets_to_rotate.append(instance)

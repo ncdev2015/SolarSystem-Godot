a3b5fbbf24476ec5da1e24953ff36f0ee55890d1
ncdev2015 <ncdev2015@gmail.com>
2021-10-25 09:30:50 -0300

    Updated: orbits for each planet

--- main/main.gd
+++ main/main.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var planets: Array
 var positions: Array
-var planets_to_rotate: Array
+var planets_in_scene: Array
 
 var mercury = preload("res://planets/mercury.tscn")
 var venus = preload("res://planets/venus.tscn")
@@ -13,54 +13,68 @@ var saturn = preload("res://planets/saturn.tscn")
 var neptune = preload("res://planets/neptune.tscn")
 var uranus = preload("res://planets/uranus.tscn")
 
-var sun_rotation_velocity = 0.25
+var sun_rotation_velocity = 0.1
+
+func draw_circle_arc(center, radius, angle_from, angle_to, color):
+	var nb_points = 200
+	var points_arc = PoolVector2Array()
+
+	for i in range(nb_points + 1):
+		var angle_point = deg2rad(angle_from + i * (angle_to-angle_from) / nb_points - 90)
+		points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+
+	for index_point in range(nb_points):
+		draw_line(points_arc[index_point], points_arc[index_point + 1], color)
+
+func draw_orbit(radius = 1000):
+	draw_circle_arc($Sun.position, radius, 0, 360, Color(1.0, 0.0, 0.0))
+
+func get_distance_to( pointA, pointB ):
+	return sqrt(pow(pointA.x-pointB.x, 2) + pow(pointA.y-pointB.y, 2))
 
 func _ready():
 	planets = [
-		mercury,
-		venus,
-		earth,
-		mars,
-		jupiter,
-		saturn,
-		neptune,
-		uranus
-	]
-	
-	positions = [
-		$Planets/MercuryPosition,
-		$Planets/VenusPosition,
-		$Planets/EarthPosition,
-		$Planets/MarsPosition,
-		$Planets/JupiterPosition,
-		$Planets/SaturnPosition,
-		$Planets/NeptunePosition,
-		$Planets/UranusPosition
+		{ "planet": mercury, "position": $Postions/Mercury },
+		{ "planet": venus, "position": $Postions/Venus },
+		{ "planet": earth, "position": $Postions/Earth },
+		{ "planet": mars, "position": $Postions/Mars },
+		{ "planet": jupiter, "position": $Postions/Jupiter },
+		{ "planet": saturn, "position": $Postions/Saturn },
+		{ "planet": neptune, "position": $Postions/Neptune },
+		{ "planet": uranus, "position": $Postions/Uranus }
 	]
 
 func _process(delta):
 	$Sun.rotate(delta * sun_rotation_velocity)
-	for planet in planets_to_rotate:
-		# Add rotation
-		pass
+	update() # for update canvas per every frame
 
 func _unhandled_input(event):
 	if event is InputEventKey:
-		if event.pressed and event.scancode == KEY_SPACE and not event.echo:
-			var next_planet = get_next_planet()
-			if next_planet["planet"]:
-				add_planet_to_scene(next_planet)
-			
+		if event.pressed:
+			if event.scancode == KEY_SPACE and not event.echo:
+				var next_planet = get_next_planet()
+				if next_planet["planet"]:
+					add_planet_to_scene(next_planet)
+
+			if event.scancode == KEY_ESCAPE:
+				get_tree().quit()
+
+func _draw():
+	for p in planets_in_scene:
+		draw_orbit(get_distance_to(p.position, $Sun.position))
+		
 func get_next_planet():
-	return {
-		"planet": planets.pop_front(),
-		"position": positions.pop_front()
-	}
+	return planets.pop_front()
 
 func add_planet_to_scene(planet):
 	var instance = planet["planet"].instance()
-	instance.position = planet["position"].position
+
+	# init planet
 	instance.scale = Vector2(.1,.1)
-	add_child(instance)
+	instance.position = planet["position"].position
 	
-	planets_to_rotate.append(instance)
+	# add orbit
+	planets_in_scene.append(instance)
+
+	# add to scene
+	add_child(instance)
